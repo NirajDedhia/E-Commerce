46530cb074257839009d1f9ce1926f17710081c5
LAPTOP-DJMEEQ1F\niraj <dedhianiraj1210@gmail.com>
2018-03-12 23:22:22 -0400

    Changed baseURL

--- javaScriptFile.js
+++ javaScriptFile.js
@@ -1,31 +1,31 @@
 function addToCart(productId, page) {
     var a = document.getElementById(productId+"Q").value;
-    location.replace("http://localhost/Server/Project1/"+page+"?addToCart=1&id="+productId+"&quantity="+a);
+    location.replace("http://localhost/Server/E-Commerce/"+page+"?addToCart=1&id="+productId+"&quantity="+a);
 };
 
 function deleteProductFromCart(productId, quantity) {
-    location.replace("http://localhost/Server/Project1/cart.php?delete=1&id="+productId+"&quantity="+quantity);
+    location.replace("http://localhost/Server/E-Commerce/cart.php?delete=1&id="+productId+"&quantity="+quantity);
 };
 
 function updateProductInCartCart(productId, quantity) {
     var a = document.getElementById(productId+"Q").value;
     quantity = quantity - a;
-    location.replace("http://localhost/Server/Project1/cart.php?update=1&id="+productId+"&quantity="+quantity);
+    location.replace("http://localhost/Server/E-Commerce/cart.php?update=1&id="+productId+"&quantity="+quantity);
 };
 
 function logout($page) {
-    location.replace("http://localhost/Server/Project1/"+$page+"?logout=1")
+    location.replace("http://localhost/Server/E-Commerce/"+$page+"?logout=1")
 };
 
 function updateProduct(productId, sale) {
     var p = document.getElementById(productId+"P").value;
     var q = document.getElementById(productId+"Q").value;
     var s = booleanToInt(document.getElementById(productId+"S").checked);
-    location.replace("http://localhost/Server/Project1/updateProduct.php?update=1&id="+productId+"&price="+p+"&quantity="+q+"&newSale="+s+"&oldSale="+sale);
+    location.replace("http://localhost/Server/E-Commerce/updateProduct.php?update=1&id="+productId+"&price="+p+"&quantity="+q+"&newSale="+s+"&oldSale="+sale);
 };
 
 function deleteProduct(productId, sale) {
-    location.replace("http://localhost/Server/Project1/updateProduct.php?delete=1&id="+productId+"&sale="+sale);
+    location.replace("http://localhost/Server/E-Commerce/updateProduct.php?delete=1&id="+productId+"&sale="+sale);
 };
 
 function booleanToInt(bool)
